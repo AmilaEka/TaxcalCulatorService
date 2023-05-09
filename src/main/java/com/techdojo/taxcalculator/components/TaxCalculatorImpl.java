bcf6187db29bcd15af7b848e6f16a6cd521435fc
Amila Ekanayake <amilaekanayake@Amilas-MacBook-Air.local>
2023-05-09 21:59:03 +1200

    add new test

--- src/main/java/com/techdojo/taxcalculator/components/TaxCalculatorImpl.java
+++ src/main/java/com/techdojo/taxcalculator/components/TaxCalculatorImpl.java
@@ -6,12 +6,33 @@ import org.springframework.stereotype.Component;
 public class TaxCalculatorImpl implements TaxCalculator {
 
     @Override
-    public double calculateTax(double income) {
-        if (income>180000 && income<=180001){return income*(39.0/100);}
-        if (income>70000 && income<=180000){return income*(33.0/100);}
-        if (income>48000 && income<=70000){return income*(30.0/100);}
-        if (income>14000 && income<=48000){return income*(17.5/100);}
-        if (income>0 && income<=13999){return income*(15.0/100);}
-        return income*(10.5/100);
+   public double calculateTax(double income) {
+//        if (income > 180000) {
+//            return income * (39.0 / 100);
+//        } else if (income > 70000 && income <= 180000) {
+//            return income * (33.0 / 100);
+//        } else if (income > 48000 && income <= 70000) {
+//            return income * (30.0 / 100);
+//        } else if (income > 14000 && income <= 48000) {
+//            return income * (17.5 / 100);
+//        }
+////        if (income>0 && income<=13999){return income*(15.0/100);}
+//        else {
+//            return income * (10.5 / 100);
+//        }
+        if (income > 0 && income<=14000) {
+            return income * (10.5 / 100);
+        } else if (income > 70000 && income <= 180000) {
+            return income * (33.0 / 100);
+        } else if (income > 48000 && income <= 70000) {
+            return income * (30.0 / 100);
+        } else if (income > 14000 && income <= 48000) {
+            return income * (17.5 / 100);
+        }
+//        if (income>0 && income<=13999){return income*(15.0/100);}
+        else {
+            return income * (39.0 / 100);
+        }
     }
+
 }
